b05e932c95cf3acec8b3fce1640b9750696ec4fa
Matthias Endler <matthias-endler@gmx.net>
2018-07-31 21:18:50 +0200

    Use bufsize constant

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use std::os::unix::io::AsRawFd;
 use nix::fcntl::{splice, SpliceFFlags};
 use nix::unistd::pipe;
 
+const BUF_SIZE: usize = 16384;
+
 fn main() {
     for path in env::args().skip(1) {
         let input = File::open(&path).expect(&format!("fcat: {}: No such file or directory", path));
@@ -21,7 +23,7 @@ fn main() {
                 None,
                 wr,
                 None,
-                16384,
+                BUF_SIZE,
                 SpliceFFlags::empty(),
             ).unwrap();
 
@@ -34,7 +36,7 @@ fn main() {
                 None,
                 stdout.as_raw_fd(),
                 None,
-                16384,
+                BUF_SIZE,
                 SpliceFFlags::empty(),
             ).unwrap();
         }
